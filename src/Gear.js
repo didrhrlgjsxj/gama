c45270b0bf063e45bf7d863eedd1a11495081104
didrhrlgjsxj <syj6687@gmail.com>
2025-07-21 01:41:16 +0000

    07-21 기어 추가2

--- src/Gear.js
+++ src/Gear.js
@@ -1,5 +1,5 @@
 class Gear {
-    constructor(nemo, size = nemo.size * 0.4) {
+    constructor(nemo, size = nemo.size * 0.4 * 1.5) {
         this.nemo = nemo;
         this.size = size;
         this.angle = 0;
@@ -10,15 +10,31 @@ class Gear {
         this.angle += this.nemo.activity * 0.2;
     }
 
+    getTeethCount() {
+        if (this.nemo.unitType === 'unit') return 2;
+        if (this.nemo.unitType === 'army') {
+            switch (this.nemo.armyType) {
+                case 'squad':
+                case 'sqaudio':
+                    return 3;
+                case 'platoon':
+                    return 4;
+                case 'company':
+                    return 6;
+            }
+        }
+        return 8;
+    }
+
     draw(ctx) {
         ctx.save();
         ctx.rotate(this.angle);
         const r = this.size / 2;
         const inner = r * 0.6;
-        const teeth = 8;
+        const teeth = this.getTeethCount();
         ctx.fillStyle = 'lightgray';
         ctx.strokeStyle = 'gray';
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 4;
         ctx.beginPath();
         ctx.arc(0, 0, inner, 0, Math.PI * 2);
         ctx.fill();
